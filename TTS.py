fa6ff3d92851dc477759fe041640d396bc263f4f
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-05-18 21:17:15 +0530

    Update TTS.py

--- TTS.py
+++ TTS.py
@@ -1,18 +1,20 @@
 from gtts import gTTS
 import os
 
-# Set the text to be spoken
-text = "Hello I'm your assistant, How can I assist you?"
+# Ask the user to input the text
+text = input("Please enter the text to be spoken: ")
 
 # Specify the language and gender (optional)
 language = 'en'
 gender = 'female'
 
 # Generate the speech using gTTS
-tts = gTTS(text=text, lang=language, tld='com', slow=False)
+tts = gTTS(text=text, lang=language, slow=False)
+
+# Specify the path to save the output file
+output_path = r"C:\Users\pushk\OneDrive\Desktop\Untitled\output.mp3"
 
 # Save the speech to an audio file
-output_path = r"C:\Users\pushk\OneDrive\Desktop\Untitled\output.mp3"
 tts.save(output_path)
 
 # Play the audio file (optional)
