1889953300beee553cba03a10eb699c9cec7be0c
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-05-18 21:13:08 +0530

    Update TTS.py

--- TTS.py
+++ TTS.py
@@ -1,27 +1,19 @@
-import pyttsx3
-
-# Initialize the text-to-speech engine
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-
-# Select the Microsoft Zira voice
-for voice in voices:
-    if voice.name == 'Microsoft Zira Desktop':
-        engine.setProperty('voice', voice.id)
-        break
-
-# Set the rate and volume of the speech
-rate = engine.getProperty('rate')
-engine.setProperty('rate', 150)
-volume = engine.getProperty('volume')
-engine.setProperty('volume', 1.0)
+from gtts import gTTS
+import os
 
 # Set the text to be spoken
-text = "Baby!!! muu m lelo naaa please!"
+text = "Hello I'm your assistant, How can I assist you?"
 
-# Specify the path to save the output file
+# Specify the language and gender (optional)
+language = 'en'
+gender = 'female'
+
+# Generate the speech using gTTS
+tts = gTTS(text=text, lang=language, tld='com', slow=False)
+
+# Save the speech to an audio file
 output_path = r"C:\Users\pushk\OneDrive\Desktop\Untitled\output.mp3"
+tts.save(output_path)
 
-# Convert the text to speech and save to file
-engine.save_to_file(text, output_path)
-engine.runAndWait()
+# Play the audio file (optional)
+os.system(f'start {output_path}')
